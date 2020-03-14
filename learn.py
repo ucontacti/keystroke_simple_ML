bc0587fa505001f76d517c48dc9883f2efdd7a36
sali <ucontacti2012@gmail.com>
2020-03-15 00:21:24 +0100

    added feature selection

--- learn.py
+++ learn.py
@@ -28,15 +28,60 @@ y = in_df["Label"]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 
+# In[3]: Feature Selection
+#apply SelectKBest class to extract top 10 best features
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import f_classif
+
+bestfeatures = SelectKBest(score_func=f_classif, k=10)
+fit = bestfeatures.fit(X_train,y_train)
+dfscores = pd.DataFrame(fit.scores_)
+dfcolumns = pd.DataFrame(X.columns)
+#concat two dataframes for better visualization 
+featureScores = pd.concat([dfcolumns,dfscores],axis=1)
+featureScores.columns = ['Specs','Score']  #naming the dataframe columns
+print('-------------- Univariate Selection Feature Selection----------------------------')
+print(featureScores.nlargest(10,'Score'))  #print 10 best features
+
+
+
 # In[4]: SVM classifier
 from sklearn import svm
-clf = svm.SVC(gamma='scale').fit(X_train, y_train)
-prediction_rm=clf.predict(X_test)
-print('--------------The Accuracy of the model----------------------------')
-print('The accuracy of the SVM Classifier is', round(accuracy_score(prediction_rm,y_test)*100,2))
-kfold = KFold(n_splits=10, random_state=22) # k=10, split the data into 10 equal parts
-result_rm=cross_val_score(clf, X, y, cv=10,scoring='accuracy')
-print('The cross validated score for SVM Classifier is:',round(result_rm.mean()*100,2))
+accuracy_list = []
+err_list = []
+for i in range(len(X_train.columns)):
+    X_train_feature_less = X_train.drop([X_train.columns[i]], axis='columns')    
+    X_test_feature_less = X_test.drop([X_test.columns[i]], axis='columns')    
+    clf = svm.SVC(gamma='scale').fit(X_train_feature_less, y_train)
+    prediction_rm=clf.predict(X_test_feature_less)
+    accuracy_list.append(round(accuracy_score(prediction_rm,y_test)*100,2))
+    err_list.append(round(calculate_eer(prediction_rm, y_test)*100,2))
+dfscores = pd.DataFrame(accuracy_list)
+dfcolumns = pd.DataFrame(X_train.columns)
+featureScores = pd.concat([dfcolumns,dfscores],axis=1)
+featureScores.columns = ['Specs','Score']  #naming the dataframe columns
+print('--------------Feature Selection by accuracy----------------------------')
+print(featureScores.nsmallest(10,'Score'))  #print 10 best features
+
+dfscores = pd.DataFrame(err_list)
+dfcolumns = pd.DataFrame(X_train.columns)
+featureScores = pd.concat([dfcolumns,dfscores],axis=1)
+featureScores.columns = ['Specs','Score']  #naming the dataframe columns
+print('--------------Feature Selection by eer----------------------------')
+print(featureScores.nlargest(10,'Score'))  #print 10 best features
+
+# print(featureScores.nlargest(10,'Score'))  #print 10 best features
+# print(accuracy_list)
+# print(np.argsort(accuracy_list))
+# print(err_list)
+# print(np.argsort(err_list))
+# print(accuracy_list.index(max(accuracy_list)))
+# print(err_list.index(min(err_list)))
+# print('--------------The Accuracy of the model----------------------------')
+# print('The accuracy of the SVM Classifier is', round(accuracy_score(prediction_rm,y_test)*100,2))
+# kfold = KFold(n_splits=10, random_state=22) # k=10, split the data into 10 equal parts
+# result_rm=cross_val_score(clf, X, y, cv=10,scoring='accuracy')
+# print('The cross validated score for SVM Classifier is:',round(result_rm.mean()*100,2))
 
 import joblib
 joblib.dump(clf, 'svm_model.pkl', compress=9)
